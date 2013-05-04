84e3d88acd6d0bea12072e0af0d8d07bac3bb0d9
brianhou <brian.hou@live.com>
2013-05-04 14:16:07 -0700

    wrestling with django, now have basic views

--- icicles/views.py
+++ icicles/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render, render_to_response
+
+def home(request):
+    return render_to_response('index.html', request)
