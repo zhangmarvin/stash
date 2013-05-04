4ff24d2c962c738a0d5b4db710bc0e570f253e28
derbear <derek.leung@berkeley.edu>
2013-05-04 14:13:35 -0700

    updated user model

--- icicles/models.py
+++ icicles/models.py
@@ -5,5 +5,7 @@ from django.db import models
 class User(models.Model):
     name = Model.CharField(max_length=30)
     salt = Model.CharField(max_length=5)
-    passwd = Model.CharField(max_length=50)
-    
+    passwd = Model.CharField(max_length=50) # hashed
+    stashes = Model.ManyToManyField(Stash)
+    viewed = Model.ManyToManyField(Heap)
+    curated = Model.ManyToManyField(Heap)
