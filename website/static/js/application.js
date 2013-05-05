c7fe2de66b2d01e5c815925ff035a57d350a187a
derbear <derek.leung@berkeley.edu>
2013-05-04 18:02:54 -0700

    core bindings from front to back

--- website/static/js/application.js
+++ website/static/js/application.js
@@ -26,7 +26,7 @@ function _make_success_wrapper(success_cb) {
 
 /* If there's an error doing the login POST, it calls error_cb with one of
    { 'type': <non-HTTP error> }
-   { 'type': 'http', 'reason': <server return code> }
+   { 'type': 'http', 'reason': <server return message> }
 
    Otherwise, it calls success_cb with one of the following:
    { 'success': true },
@@ -43,9 +43,29 @@ function login(username, password, success_cb, error_cb) {
     });
 }
 
+/* If there's an error doing the login POST, it calls error_cb with one of
+   { 'type': <non-HTTP error> }
+   { 'type': 'http', 'reason': <server return message> }
+
+   Otherwise, it calls success_cb with one of the following:
+   { 'success': true },
+   { 'success': false, 'reason': 'already registered' },
+   { 'success': false, 'reason': 'password required' },
+   { 'success': false, 'reason': <other reason>}.
+*/
+function register(username, password, success_cb, error_cb) {
+    $.ajax( {
+	url: 'ajax/login',
+	type: 'POST',
+	data: {'name': username, 'password': password},
+	success: _make_success_wrapper(success_cb),
+	failure: _make_error_wrapper(error_cb)
+    });
+}
+
 /* If there's an error doing the logout POST, it calls error_cb with one of
    { 'type': <non-HTTP error> }
-   { 'type': 'http', 'reason': <server return code> }
+   { 'type': 'http', 'reason': <server return message> }
 
    Otherwise, it calls success_cb with one of the following:
    { 'success': true },
@@ -61,14 +81,71 @@ function logout(username, success_cb, error_cb) {
     });
 }
 
-/* If there's an error submitting a link, it calls error_cb with one of
+/* For "stashing" a link away... 
+   
+   If there's an error submitting a link, it calls error_cb with one of
    { 'type': <non-HTTP error> }
-   { 'type': 'http', 'reason': <server return code> }
+   { 'type': 'http', 'reason': <server return message> }
 
    Otherwise, it calls success_cb with one of the following:
    { 'success': true },
    { 'success': false, 'reason': 'not logged in' },
    { 'success': false, 'reason': 'no write access' }
+   { 'success': false, 'reason': 'stash does not exist' }
 */
-//function submit_link(title, link_url, success_cb, error_cb) {
-    
+function stash_link(stash_id, title, link_url, success_cb, error_cb) {
+    $.ajax( {
+	url: 'ajax/stash_link',
+	type: 'GET',
+	data: {'stash': stash_id, 'title': title, 'url': link_url},
+	success: _make_success_wrapper(success_cb),
+	error: _make_error_wrapper(error_cb)
+    });
+}
+
+/* For "throwing a link onto a heap"... 
+   
+   If there's an error submitting a link, it calls error_cb with one of
+   { 'type': <non-HTTP error> }
+   { 'type': 'http', 'reason': <server return message> }
+
+   Otherwise, it calls success_cb with one of the following:
+   { 'success': true },
+   { 'success': false, 'reason': 'not logged in' },
+   { 'success': false, 'reason': 'no write access' }
+   { 'success': false, 'reason': 'heap does not exist' }
+*/
+function throw_link(heap_id, title, link_url, success_cb, error_cb) {
+    $.ajax( {
+	url: 'ajax/throw_link',
+	type: 'GET',
+	data: {'heap': heap_id, 'title': title, 'url': link_url},
+	success: _make_success_wrapper(success_cb),
+	error: _make_error_wrapper(error_cb)
+    });
+}
+
+/* Toggle whether or not a stash takes gives from a certain heap.
+
+   If there's an error setting changing the stash, it calls error_cb with one of
+   { 'type': <non-HTTP error> }
+   { 'type': 'http', 'reason': <server return message> }
+
+   Otherwise, it calls success_cb with one of the following:
+   { 'success': true , 'result': true/false},                <-- 'result' is the new value
+   { 'success': false, 'reason': 'not logged in'},
+   { 'success': false, 'reason': 'not your stash bro'},
+   { 'success': false, 'reason': 'no read access to heap'}
+   { 'success': false, 'reason': 'heap does not exist' }
+   { 'success': false, 'reason': 'stash does not exist' }
+*/
+function stash_take_toggle(stash_id, heap_id, link_url, success_cb, error_cb) {
+    $.ajax( {
+	url: 'ajax/toggle_take',
+	type: 'GET',
+	data: {'stash': stash_id, 'heap': heap_id, 'url': link_url},
+	success: _make_success_wrapper(success_cb),
+	error: _make_error_wrapper(error_cb)
+    });
+}
+
